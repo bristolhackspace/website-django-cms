83c9cb909e4339db17249ed4f2ef04e154a1081c
Aaron Shrimpton <aaronds@gmail.com>
2026-03-12 22:49:21 +0000

    Push live date tag

--- container-push-live.sh
+++ container-push-live.sh
@@ -2,7 +2,11 @@
 
 CONTAINER_CMD=$(which podman || which docker)
 
+DATE_TAG=$(date +%Y%m%d)
+
 $CONTAINER_CMD tag registry.bristolhackspace.org/website-django-cms:staging registry.bristolhackspace.org/website-django-cms:live
+$CONTAINER_CMD tag registry.bristolhackspace.org/website-django-cms:staging registry.bristolhackspace.org/website-django-cms:$DATE_TAG
 
 $CONTAINER_CMD push registry.bristolhackspace.org/website-django-cms:live
+$CONTAINER_CMD push registry.bristolhackspace.org/website-django-cms:$DATE_TAG
 
